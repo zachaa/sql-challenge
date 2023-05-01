7bf4de24f6ad0b8ab48c125b1fa5bfbb6d879871
zachaa <57045291+zachaa@users.noreply.github.com>
2023-05-01 12:28:27 -0500

    change primary keys and not nulls

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
+
 CREATE TABLE departments(
     dept_no VARCHAR(4) PRIMARY KEY,
     dept_name TEXT NOT NULL
@@ -12,33 +19,33 @@ CREATE TABLE employees(
     emp_no INT PRIMARY KEY,
 	emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE NOT NULL,
-    first_name TEXT,
-    last_name TEXT,
-    sex VARCHAR(1),
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    sex VARCHAR(1) NOT NULL,
     hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 -- below 3 tables need to be created after the above 3
 CREATE TABLE dept_emp(
-    id SERIAL PRIMARY KEY,      -- Employees in a department can change, so we don't want a composite key
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-    id SERIAL PRIMARY KEY,      -- Department managers can change, so we don't want a composite key
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries(
-    id SERIAL PRIMARY KEY,      -- Salaries can change, so we don't want a composite key
 	emp_no INT NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    salary INT NOT NULL
+    salary INT NOT NULL,
+    PRIMARY KEY (emp_no, salary),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
